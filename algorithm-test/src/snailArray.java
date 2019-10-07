8b145835418574341811a3204d4efeea3d88237b
JeongyeonRo <47593108+JeongyeonRo@users.noreply.github.com>
2019-10-07 22:11:12 +0900

    practice java
    practice java

---
+++ algorithm-test/src/snailArray.java
@@ -0,0 +1,33 @@
+
+public class snailArray {
+
+	public static void main(String[] args) {
+		int A [][] = new int[5][5];
+		int N = 0, S=1, i=0, j=-1, K=5;
+		while(true) {
+			for(int P=0; P<K; P++) {
+				N=N+1;
+				j=j+S;
+				A[i][j] = N;
+			}
+			K=K -1;
+			if(K <= 0)
+				break;
+			for(int P=0; P<K; P++) {
+				N=N+1;
+				i=i+S;
+				A[i][j] = N;
+			}
+			S = S * (-1);
+		}
+		//배열 A[5][5] 출력
+		for(i=0; i<5; i++) {
+			for(j=0;j<5;j++) {
+				System.out.print(A[i][j]);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
